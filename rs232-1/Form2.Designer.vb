c46a6d0d47a4a2082575afa268e6fefd2f2cf6c8
Jonny Chung <lico@livemail.tw>
2022-01-19 12:50:57 +0800

    Add files via upload

---
+++ rs232-1/Form2.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(295, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(166, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "輸入數字 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(295, 177)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(166, 32)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "輸入數字 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label3.Location = New System.Drawing.Point(295, 375)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(166, 32)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "讀取數字 1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label4.Location = New System.Drawing.Point(295, 501)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(166, 32)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "讀取數字 2"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("新細明體", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(281, 75)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(203, 94)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.Handshake = System.IO.Ports.Handshake.XOnXOff
+        Me.SerialPort1.ReadTimeout = 10
+        Me.SerialPort1.WriteTimeout = 10
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("新細明體", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(281, 225)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(203, 94)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label5
+        '
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label5.Font = New System.Drawing.Font("新細明體", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label5.Location = New System.Drawing.Point(281, 420)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(203, 70)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "0"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label6.Font = New System.Drawing.Font("新細明體", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label6.Location = New System.Drawing.Point(281, 545)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(203, 70)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "0"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button1.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.Location = New System.Drawing.Point(86, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 60)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "連線"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button2.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button2.Location = New System.Drawing.Point(86, 114)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(95, 60)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "斷線"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button3.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button3.Location = New System.Drawing.Point(490, 121)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 145)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "傳送"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button4.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button4.Location = New System.Drawing.Point(490, 445)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 145)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "接收"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label7.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 194)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(263, 421)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "斷線"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(577, 639)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "PC<->HMI Test"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label7 As Label
+End Class
---
+++ rs232-1/Form2.vb
@@ -0,0 +1,151 @@
+﻿Public Class Form1
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            'SerialPort1.PortName = cmbCOM.SelectedItem.ToString '設定PortName的值
+            SerialPort1.ReceivedBytesThreshold = 1
+            SerialPort1.ReadTimeout = 1000
+            SerialPort1.Open() '開啟SerialPort
+            Label7.Text = "已連線"
+        Catch ex As Exception
+            Label7.Text = ex.ToString()
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SerialPort1.Close() '關閉SerialPort
+        Label7.Text = "斷線中"
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim buff() As Byte
+        Dim s As String = ""
+        Dim j As Integer
+
+        ReDim buff(SerialPort1.BytesToRead - 1)
+        SerialPort1.Read(buff, 0, buff.Length)
+        For i As Integer = 0 To buff.Length - 1
+            s += buff(i).ToString("X2")
+            If j < 22 Then
+                j = j + 1
+            End If
+        Next
+        Dim returnStr As String = s & vbCrLf
+        'Return returnStr
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Dim Output As String = (":" + "01030000" + LRC("010300000001") + Chr(13) + Chr(10))
+        'Label5.Text = SerialPort1.Encoding("get") 'As System.Text.Encoding
+        'TextBox2.Text = TextBox2.Text & s & vbCrLf
+        Dim tString As String = "01030000001"
+        'Dim tString As String = "01039C410002"
+        'Dim tString As String = "010675310002"
+        Dim TempString As String = ":" + tString + LRC(tString) + Chr(13) + Chr(10)
+        ModbusWrite(TempString)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        'AxMSComm1.Output = ":" + "010300020002" + LRC("010300020002") + Chr(13) + Chr(10)
+        'Dim tString As String = AxMSComm1.Input
+        Dim tString As String = "01030000001"
+        'Dim tString As String = "01039C410002"
+        'Dim tString As String = "010375310002"
+        Dim TempString As String = ":" + tString + LRC(tString) + Chr(13) + Chr(10)
+        SerialPort1.Write(TempString)
+        'SerialPort1.Read(TempString)
+        'ModbusWrite(TempString)
+        TempString = ModbusRead() '= SerialPort1.ReadExisting()
+        Label7.Text = TempString
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Public Function ModbusWrite(str As String) As String
+        Dim l As Integer
+        Dim buf As Byte()
+        l = Len(str)
+        buf = SerialPort1.Encoding.ASCII.GetBytes(str)
+        'SerialPort1.Write(buf, 0, l)
+        SerialPort1.Write(str)
+    End Function
+
+    Public Function ModbusRead() As String
+        Dim str As String
+        Dim daBUF() As Byte
+        Dim n As Integer, i As Integer
+        SerialPort1.ReadTimeout = 1000
+        n = SerialPort1.BytesToRead
+        If n > 0 Then
+            ReDim daBUF(n - 1)
+            n = SerialPort1.Read(daBUF, 0, SerialPort1.BytesToRead)
+            '// 以下列函數將取得之陣列資料轉為字串格式
+            str = SerialPort1.Encoding.ASCII.GetString(daBUF)
+
+            '// 或使用下列程序將取得之陣列資料轉為字串格式
+            'str = ""
+            'For i = 0 To n - 1
+            'str = str & Chr(Val(daBUF(i)))
+            'Next
+            '// 提示資料
+            'txtRecv.Text = str
+            Return str
+        End If
+    End Function
+
+    Public Function LRC(str As String) As String
+        Dim c As Integer = 0
+        Dim l As Integer = Len(str)
+        Dim c_data As String
+        Dim d_lrc As String = ""
+        Dim h_lrc As String
+
+        For c = c + 1 To l
+            c_data = Mid(str, c, 2)
+            d_lrc = d_lrc + TxtToHex(c_data)
+            c = c + 1
+        Next c
+        If d_lrc > &HFF Then
+            d_lrc = d_lrc Mod &H100
+        End If
+        h_lrc = Hex(&HFF - d_lrc + 1)
+        If Len(h_lrc) > 2 Then
+            h_lrc = Mid(h_lrc, Len(h_lrc) - 1, 2)
+        End If
+        LRC = h_lrc
+    End Function
+
+    Public Function TxtToHex(ByVal s As String) As String
+        Dim sstr As String, l As Integer, ss As String
+        sstr = ""
+        For l = 1 To Len(s)
+            ss = Hex(Asc(Mid(s, l, 1)))
+            If Len(ss) = 1 Then ss = "0" & ss
+            sstr = sstr & ss
+        Next
+        TxtToHex = sstr
+    End Function
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
